277548d8a08389d496667e15f3eec6a28ed884fd
Sebastian Waisbrot <seppo0010@gmail.com>
2020-04-23 09:14:13 -0300

    Odia content kickoff

--- components/Main.js
+++ components/Main.js
@@ -13,63 +13,18 @@ class Main extends React.Component {
     return (
       <div id="main" style={this.props.timeout ? {display: 'flex'} : {display: 'none'}}>
 
-        <article id="intro" className={`${this.props.article === 'intro' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''}`} style={{display:'none'}}>
-          <h2 className="major">Intro</h2>
-          <span className="image main"><img src="/static/images/pic01.jpg" alt="" /></span>
-          <p>Aenean ornare velit lacus, ac varius enim ullamcorper eu. Proin aliquam facilisis ante interdum congue. Integer mollis, nisl amet convallis, porttitor magna ullamcorper, amet egestas mauris. Ut magna finibus nisi nec lacinia. Nam maximus erat id euismod egestas. By the way, check out my <a href="#work">awesome work</a>.</p>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis dapibus rutrum facilisis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Etiam tristique libero eu nibh porttitor fermentum. Nullam venenatis erat id vehicula viverra. Nunc ultrices eros ut ultricies condimentum. Mauris risus lacus, blandit sit amet venenatis non, bibendum vitae dolor. Nunc lorem mauris, fringilla in aliquam at, euismod in lectus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. In non lorem sit amet elit placerat maximus. Pellentesque aliquam maximus risus, vel sed vehicula.</p>
+        <article id="quienes_somos" className={`${this.props.article === 'quienes_somos' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''}`} style={{display:'none'}}>
+          <h2 className="major">&iquest;Quiénes somos?</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In ut consectetur nunc, a consectetur eros. Etiam sodales non mauris non varius. Etiam metus sem, luctus sit amet mauris ut, dictum ornare urna. Aliquam erat volutpat. In sed dui ex. Aliquam commodo metus a est sodales, nec commodo urna consectetur. Donec eleifend tellus eget feugiat suscipit. Nulla facilisi. Praesent lacus ex, molestie non sem eget, cursus pharetra mi.</p>
+          <p>Mauris urna orci, tristique eu rutrum id, vestibulum ac neque. Mauris sodales venenatis scelerisque. In viverra sem ex, in facilisis ex ullamcorper nec. Cras eu cursus sapien. Fusce luctus dapibus orci, eget molestie nunc malesuada at. Etiam ac orci nunc. Suspendisse a tellus non nisl condimentum aliquet. Sed quis tempus eros. Nulla in feugiat neque. Fusce dapibus turpis fermentum sem euismod pellentesque a id purus. Pellentesque sed nulla neque. Duis ultrices consequat mauris, et porttitor purus pulvinar tempor. Mauris fringilla libero sed massa consequat tincidunt. Vivamus cursus accumsan neque, eget maximus erat vestibulum sit amet.</p>
+          <p>Integer imperdiet ipsum a mi vestibulum, porttitor efficitur enim fermentum. Phasellus convallis sapien in nibh varius, ut ultrices magna rhoncus. In at augue id nisi condimentum tempor. Pellentesque eget nulla ac urna eleifend luctus a vitae dui. Sed dictum malesuada nulla, sed rutrum justo aliquam sit amet. Nulla eget libero turpis. Etiam ultricies diam ac nisl efficitur vestibulum non posuere lacus. Donec turpis mauris, pretium sit amet metus sit amet, imperdiet tempus nisl. Quisque diam urna, interdum quis nulla a, luctus consectetur tortor. Cras pharetra felis a orci molestie, imperdiet faucibus elit semper. Nulla in tortor neque. Ut sem purus, dapibus at tempus vel, sodales sed leo. Suspendisse venenatis tincidunt augue, eget blandit massa eleifend eu.</p>
           {close}
         </article>
 
-        <article id="work" className={`${this.props.article === 'work' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''}`} style={{display:'none'}}>
-          <h2 className="major">Work</h2>
-          <span className="image main"><img src="/static/images/pic02.jpg" alt="" /></span>
-          <p>Adipiscing magna sed dolor elit. Praesent eleifend dignissim arcu, at eleifend sapien imperdiet ac. Aliquam erat volutpat. Praesent urna nisi, fringila lorem et vehicula lacinia quam. Integer sollicitudin mauris nec lorem luctus ultrices.</p>
-          <p>Nullam et orci eu lorem consequat tincidunt vivamus et sagittis libero. Mauris aliquet magna magna sed nunc rhoncus pharetra. Pellentesque condimentum sem. In efficitur ligula tate urna. Maecenas laoreet massa vel lacinia pellentesque lorem ipsum dolor. Nullam et orci eu lorem consequat tincidunt. Vivamus et sagittis libero. Mauris aliquet magna magna sed nunc rhoncus amet feugiat tempus.</p>
-          {close}
-        </article>
-
-        <article id="about" className={`${this.props.article === 'about' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''}`} style={{display:'none'}}>
-          <h2 className="major">About</h2>
-          <span className="image main"><img src="/static/images/pic03.jpg" alt="" /></span>
-          <p>Lorem ipsum dolor sit amet, consectetur et adipiscing elit. Praesent eleifend dignissim arcu, at eleifend sapien imperdiet ac. Aliquam erat volutpat. Praesent urna nisi, fringila lorem et vehicula lacinia quam. Integer sollicitudin mauris nec lorem luctus ultrices. Aliquam libero et malesuada fames ac ante ipsum primis in faucibus. Cras viverra ligula sit amet ex mollis mattis lorem ipsum dolor sit amet.</p>
-          {close}
-        </article>
-
-        <article id="contact" className={`${this.props.article === 'contact' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''}`} style={{display:'none'}}>
-          <h2 className="major">Contact</h2>
-          <form method="post" action="#">
-            <div className="field half first">
-              <label htmlFor="name">Name</label>
-              <input type="text" name="name" id="name" />
-            </div>
-            <div className="field half">
-              <label htmlFor="email">Email</label>
-              <input type="text" name="email" id="email" />
-            </div>
-            <div className="field">
-              <label htmlFor="message">Message</label>
-              <textarea name="message" id="message" rows="4"></textarea>
-            </div>
-            <ul className="actions">
-              <li><input type="submit" value="Send Message" className="special" /></li>
-              <li><input type="reset" value="Reset" /></li>
-            </ul>
-          </form>
-          <ul className="icons">
-            <li><a href="#">
-              <FontAwesomeIcon icon={faTwitter} />
-            </a></li>
-            <li><a href="#">
-              <FontAwesomeIcon icon={faFacebook} />
-            </a></li>
-            <li><a href="#">
-              <FontAwesomeIcon icon={faInstagram} />
-            </a></li>
-            <li><a href="#">
-              <FontAwesomeIcon icon={faGithub} />
-            </a></li>
-          </ul>
+        <article id="proyectos" className={`${this.props.article === 'proyectos' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''}`} style={{display:'none'}}>
+          <h2 className="major">Proyectos</h2>
+          <p>Fusce quis finibus lorem. Duis blandit in augue quis tincidunt. Etiam eget sem a nunc consectetur pulvinar. Duis sed turpis dictum, accumsan turpis eget, pellentesque ipsum. Duis laoreet mauris mauris, non euismod dolor pharetra eu. Fusce sed dictum arcu. Proin elementum justo at dolor fermentum consectetur. Suspendisse lobortis, tellus vel luctus imperdiet, est dui tempor est, vel convallis orci lacus nec purus. Pellentesque eu porttitor nisl. Proin efficitur enim porttitor nulla eleifend, sed gravida eros maximus. Etiam purus justo, pharetra quis maximus nec, mollis eu ipsum. Aenean vitae ipsum vitae lacus malesuada feugiat a sed nulla. Aenean volutpat felis ut sodales ornare. Nulla facilisi.</p>
+          <p>Nunc in eleifend ex. Sed est magna, bibendum nec nunc in, fringilla pharetra massa. Mauris sed nibh eu ipsum accumsan vestibulum non nec arcu. Praesent finibus orci nisl, eget maximus enim maximus in. Suspendisse diam eros, tempus ut laoreet sed, ultrices ac diam. Phasellus est velit, ultrices eget commodo eget, vulputate pulvinar turpis. Suspendisse sit amet vulputate urna. Etiam nec malesuada sem. Nulla interdum mi sed rutrum aliquam. Proin non ex in neque vehicula aliquam eu et ligula. Donec luctus accumsan risus, vitae imperdiet nunc cursus vitae.</p>
           {close}
         </article>
 
